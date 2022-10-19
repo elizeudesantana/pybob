1a48a9240369a95eaca330063f4facdeee9fe375
elizeudesantana <elizeudesantana@hotmail.com>
2022-10-19 10:36:38 -0300

    acertando versionamentos das packges

--- bob/bob.py
+++ bob/bob.py
@@ -6,6 +6,21 @@ scriptFileVersion="0.1.0"
 History:
     2022.01     Script inicial versao, implementa a chamada
                 da unidade interface main; cria hooks.
+    2022.05.01  em : 18/10/2022
+                Inserido menus, versionado o GUI para o pyside6
+                que gerou incompatibilidade:
+                .   Virtualenv com python 3.10.8 instalado atraves do poetry
+                    usando o artificil de setar em [depencies] do toml
+                    a var pyside6 = {PySide6.2.4.whl} baixado de
+                    https://download.qt.io/official_releases/QtForPython/pyside6/
+                    e a dependencia shiboken6.
+                    Que gerou conflito, com o sistema operacional ubuntu 18.04,
+                    na libc [GLIB-2.27], era necessario [GLIBC-2.28], onde a 
+                    unica solucao era o update para ubuntu 20.01 LTS.
+                em : 19/10/2022
+                Tentativa de solucionar o bug da GUI, utilizando pyenv instalado
+                a versao python 3.9.12 criar uma nova virtualenv, e instalar o
+                pyside6.0.0
 
 Extern Dependency :
                 pyside2
