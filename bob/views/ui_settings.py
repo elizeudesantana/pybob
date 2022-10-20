8cfe1367a42aedb81c957b7428a65053dc3bbc25
elizeudesantana <elizeudesantana@hotmail.com>
2022-10-20 14:07:19 -0300

    settings

--- bob/views/ui_settings.py
+++ bob/views/ui_settings.py
@@ -17,6 +17,12 @@ Description:
 
 by: Elizeu de Santana  In: 20/10/2022
 """
+# import sys
+# import os
+
+# SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+# sys.path.append(os.path.dirname(SCRIPT_DIR))
+
 from .ui_model.toolbarinit import InitToolbar
 from .ui_model.menu.menu_main import MenuMain
 from .ui_model.modelo_clima import Clima
@@ -24,44 +30,51 @@ from .ui_model.modelo_clima import Clima
 
 class SettingsMain():
     def settings(self):
-        self.ui_maximiza(self)
-        self.ui_background()
-        self.ui_title()
-        self .ui_menus()
-        self.ui_clima()
-        self.ui_statusbar()
-
-    def ui_maximiza(self):
+    # def ui_maximiza(self):
         '''Maximizar a Unidade de Interface.'''
         self.showMaximized()
 
-    def ui_background(self):
+    # def ui_background(self):
         '''Carrega image para o background.'''
         # mg = os.path.join(CURRENT_DIR, "imagem/bg2.jpg")
         # self.setCentralWidget(StackedWidget())
         ...
 
-    def ui_title(self):
+    # def ui_title(self):
+        '''Carrega o titulo na janela.'''
         self.setWindowTitle('PyBob - EliSoftWare®')
 
-    def ui_menus(self):
+    # def ui_menus(self):
+        '''Inicializa os menus.'''
         InitToolbar.inittoolbar(self)
         MenuMain.monta_menu(self)
 
-    def ui_clima(self):
-        '''Atualiza previsao do tempo'''
-        Clima.atualiza(self)
+    # def ui_clima(self):
+        '''Atualiza previsao do tempo.'''
+        # Clima.atualiza(self)
 
-    def ui_statusbar(self):
+    # def ui_statusbar(self):
+        '''Atualiza a barra de status.'''
         self.status = self.statusBar()
         self.status.showMessage('Seja bem vindo - PyBob - EliSoftWare®')
 
-    def togglebar(self):
-        state = self.formatbar.isVisible()
-        # Set the visibility to its inverse
-        self.formatbar.setVisible(not state)
+    # def togglebar(self):
+        '''...'''
+        # state = self.formatbar.isVisible()
+        # # Set the visibility to its inverse
+        # self.formatbar.setVisible(not state)
 
-    def toggleformatbar(self):
-        state = self.formatbar.isVisible()
-        # Set the visibility to its inverse
-        self.formatbar.setVisible(not state)
+    # def toggleformatbar(self):
+        '''...'''
+        # state = self.formatbar.isVisible()
+        # # Set the visibility to its inverse
+        # self.formatbar.setVisible(not state)
+
+    #   
+    #     ''''Recebe -> UiMain(QMainWindow)'''
+    #     self.ui_maximiza()
+    #     self.ui_background()
+    #     self.ui_title()
+    #     self.ui_menus()
+    #     self.ui_clima()
+    #     self.ui_statusbar()
